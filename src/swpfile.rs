26d5917d6fe67aa3adf8ae7c2e4102a5969d2e36
Sergey Mishin <sergei.a.mishin@gmail.com>
2021-11-12 05:08:56 +0300

    Reformat: use spaces instead of tabs

--- src/swpfile.rs
+++ src/swpfile.rs
@@ -2,52 +2,51 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use std::path::Path;
 
-pub fn parse_swpfile<P: AsRef<Path>>(dir: P, filename : P) -> Result<Vec<String>> {
-	let file = File::open(dir.as_ref().join(filename))?;
-	let reader = BufReader::new(file);
+pub fn parse_swpfile<P: AsRef<Path>>(dir: P, filename: P) -> Result<Vec<String>> {
+    let file = File::open(dir.as_ref().join(filename))?;
+    let reader = BufReader::new(file);
 
-	let mut paths = Vec::new();
+    let mut paths = Vec::new();
 
-	for line in reader.lines() {
-		let line = line?.trim().to_owned();
+    for line in reader.lines() {
+        let line = line?.trim().to_owned();
 
-		if line.len() < 1 {
-			continue;
-		}
-		if line.starts_with('#') {
-			continue;
-		}
-		if paths.contains(&line) {
-			continue;
-		}
+        if line.len() < 1 {
+            continue;
+        }
+        if line.starts_with('#') {
+            continue;
+        }
+        if paths.contains(&line) {
+            continue;
+        }
 
-		paths.push(line);
-	}
+        paths.push(line);
+    }
 
-	Ok(paths)
+    Ok(paths)
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
-	use crate::utils::test_utils;
+    use super::*;
+    use crate::utils::test_utils;
 
-	#[test]
-	fn empty_file() {
-		test_utils::with_temp_dir(|dir| {
-			std::fs::write(dir.join(".swpfile"), "")
-				.expect("Could not write test file");
+    #[test]
+    fn empty_file() {
+        test_utils::with_temp_dir(|dir| {
+            std::fs::write(dir.join(".swpfile"), "").expect("Could not write test file");
 
-			let dirs = parse_swpfile(dir, Path::new(".swpfile"))
-				.expect("Error while reading .swpfile");
+            let dirs =
+                parse_swpfile(dir, Path::new(".swpfile")).expect("Error while reading .swpfile");
 
-			assert_eq!(dirs.len(), 0);
-		});
-	}
+            assert_eq!(dirs.len(), 0);
+        });
+    }
 
-	#[test]
-	fn swpfile() {
-		const FILE_CONTENTS: &'static str = r"
+    #[test]
+    fn swpfile() {
+        const FILE_CONTENTS: &'static str = r"
 			# comment
 
 			target
@@ -57,27 +56,25 @@ mod test {
 			duplicate
 			";
 
-		test_utils::with_temp_dir(|dir| {
-			std::fs::write(dir.join(".swpfile"), FILE_CONTENTS)
-				.expect("Could not write test file");
+        test_utils::with_temp_dir(|dir| {
+            std::fs::write(dir.join(".swpfile"), FILE_CONTENTS).expect("Could not write test file");
 
-			let dirs = parse_swpfile(dir, Path::new(".swpfile"))
-				.expect("Error while reading .swpfile");
+            let dirs =
+                parse_swpfile(dir, Path::new(".swpfile")).expect("Error while reading .swpfile");
 
-			assert_eq!(dirs.len(), 3);
-		});
-	}
+            assert_eq!(dirs.len(), 3);
+        });
+    }
 
-	#[test]
-	fn swpfile_cleanuprc() {
-		test_utils::with_temp_dir(|dir| {
-			std::fs::write(dir.join(".cleanuprc"), "target")
-				.expect("Could not write test file");
+    #[test]
+    fn swpfile_cleanuprc() {
+        test_utils::with_temp_dir(|dir| {
+            std::fs::write(dir.join(".cleanuprc"), "target").expect("Could not write test file");
 
-			let dirs = parse_swpfile(dir, Path::new(".cleanuprc"))
-				.expect("Error while reading .cleanuprc");
+            let dirs = parse_swpfile(dir, Path::new(".cleanuprc"))
+                .expect("Error while reading .cleanuprc");
 
-			assert_eq!(dirs.len(), 1);
-		});
-	}
+            assert_eq!(dirs.len(), 1);
+        });
+    }
 }
